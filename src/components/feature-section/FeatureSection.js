8307e663ce4df564c0b2684fc045b64da52b7a7b
Maisie <maisieobb@googlemail.com>
2024-03-28 17:48:56 +0000

    styling feature section

--- src/components/feature-section/FeatureSection.js
+++ src/components/feature-section/FeatureSection.js
@@ -1,7 +1,7 @@
 // FeatureSection.js
 
 import React from "react";
-import FeatureCard from "./FeatureCard.js"
+import FeatureCard from "./FeatureCard.js";
 import "./FeatureSection.css";
 
 let FeatureSection = () => {
@@ -9,30 +9,66 @@ let FeatureSection = () => {
     <section className="feature-section">
       <div className="feature-section__top">
         <h1 className="feature-section__title">Features</h1>
-        <p className="feature-section__description">From edge locations world wide, to unparalleled security and DDOS protection, we’ve got you covered. </p>
+        <p className="feature-section__description">
+          From edge locations world wide, to unparalleled security and DDOS
+          protection, we’ve got you covered.{" "}
+        </p>
       </div>
       <div className="feature-section__cards">
-        <FeatureCard 
-          title="EdgeNetwork" 
-          description="Discover our industry leading edge network, with over 300 edge nodes scattered around the globe.
-Give your customers the best performance in every corner of the world." 
-          imageUrl="/imgs/edge-network-img.PNG" 
+        <FeatureCard
+          title="Edge Network"
+          description={
+            <>
+              <span>
+                Discover our industry leading edge network, with over 300 edge
+                nodes scattered around the globe.
+              </span>
+              <br />
+              <br />
+              <span>
+                Give your customers the best performance in every corner of the
+                world.
+              </span>
+            </>
+          }
+          imageUrl="/imgs/edge-network-img.PNG"
         />
         <FeatureCard
-          title="Security" 
-          description="Stay safe from threats without slowing down.
-          SuperNet surrounds and protects your entire ecosystem — clouds, apps, APIs, and users." 
-          imageUrl="/imgs/security-img.PNG" 
+          title="Security"
+          description={
+            <span>
+              Stay safe from threats without slowing down.
+              <br /><br/>
+              SuperNet surrounds and protects your entire ecosystem — clouds,
+              apps, APIs, and users.
+            </span>
+          }
+          imageUrl="/imgs/security-img.PNG"
         />
-        <FeatureCard 
-          title="DDoS Protection" 
-          description="Highly rated web, application & network DDoS protection. Keeping bots and malicious users at bay." 
-          imageUrl="imgs/DDOS-img.PNG" 
+
+        <FeatureCard
+          title="DDoS Protection"
+          description={
+            <span>
+              Highly rated web, application & network DDoS protection.
+              <br /><br/>
+              Keeping bots and malicious users at bay.
+            </span>
+          }
+          imageUrl="imgs/DDOS-img.PNG"
         />
-        <FeatureCard 
-          title="Backups" 
-          description="Fully managed automatic daily, weekly, and biweekly backups of your Compute Instances. Have peace of mind knowing that your data is always safe." 
-          imageUrl="/imgs/backups.img.PNG" 
+
+        <FeatureCard
+          title="Backups"
+          description={
+            <span>
+              Fully managed automatic daily, weekly, and biweekly backups of
+              your Compute Instances.
+              <br /><br/>
+              Have peace of mind knowing that your data is always safe.
+            </span>
+          }
+          imageUrl="/imgs/backups-img.PNG"
         />
       </div>
     </section>
--- src/components/feature-section/featureCard.js
+++ src/components/feature-section/featureCard.js
@@ -6,9 +6,12 @@ import "./FeatureCard.css";
 let FeatureCard = ({ title, description, imageUrl }) => {
   return (
     <div className="feature-card">
-      <img src={imageUrl} alt={title} className="feature-card__image" />
+      <img src={imageUrl} alt={title} className="feature-card__image"
+       />
+       <div className="feature-card__text-content">
       <h3 className="feature-card__title">{title}</h3>
       <p className="feature-card__description">{description}</p>
+      </div>
     </div>
   );
 };
