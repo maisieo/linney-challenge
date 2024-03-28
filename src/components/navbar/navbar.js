9e3fdd14de17df36a76e77aee70a8fd2fa4b07c4
Maisie <maisieobb@googlemail.com>
2024-03-28 13:35:25 +0000

    style navbar desktop

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,24 @@
 // Navbar.js
 
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 
 let Navbar = () => {
+  let [isOpen, setIsOpen] = useState(false);
+
+  let toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  let toggleClass = isOpen ? "navbar__toggle open" : "navbar__toggle";
+
   return (
     <nav className="navbar">
+      <div className={toggleClass} id="navbarToggle" onClick={toggleNavbar}>
+        <div className="navbar__line"></div>
+        <div className="navbar__line"></div>
+        <div className="navbar__line"></div>
+      </div>
       <p className="navbar__paragraph">Test</p>
       <div className="navbar__nav-items">
         <ul className="navbar__nav-links">
@@ -19,13 +32,13 @@ let Navbar = () => {
               Edge Network
             </a>
           </li>
-          <li className="navbar__navItem">
+          <li className="navbar__nav-item">
             <a href="/" className="navbar__link">
               Pricing
             </a>
           </li>
         </ul>
-        <button className="navbar__signUpButton">Sign Up</button>
+        <button className="navbar__sign-up-button">Sign Up</button>
       </div>
     </nav>
   );
