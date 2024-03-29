4611ce1d6ab44113e45ccbae8055031b34d937a6
Maisie <maisieobb@googlemail.com>
2024-03-29 13:54:26 +0000

    remove 'js' for fix

--- src/components/feature-section/FeatureSection.js
+++ src/components/feature-section/FeatureSection.js
@@ -1,7 +1,7 @@
 // FeatureSection.js
 
 import React from "react";
-import FeatureCard from "./FeatureCard.js";
+import FeatureCard from "./FeatureCard";
 import "./FeatureSection.css";
 
 let FeatureSection = () => {
--- src/components/feature-section/featureCard.js
+++ src/components/feature-section/featureCard.js
@@ -6,11 +6,11 @@ import "./FeatureCard.css";
 let FeatureCard = ({ title, description, imageUrl }) => {
   return (
     <div className="feature-card">
-      <img src={imageUrl} alt={title} className="feature-card__image"
+      <img src={imageUrl} alt={`Image for ${title}`} className="feature-card__image"
        />
        <div className="feature-card__text-content">
       <h3 className="feature-card__title">{title}</h3>
-      <p className="feature-card__description" dangerouslySetInnerHTML={{ __html: description }}></p>
+      <p className="feature-card__description">{description}</p>
       </div>
     </div>
   );
