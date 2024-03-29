df3c80967d0fd54f521b924758eb04ac224948c6
Maisie <maisieobb@googlemail.com>
2024-03-29 13:47:40 +0000

    add potential fix

--- src/components/feature-section/FeatureSection.js
+++ src/components/feature-section/FeatureSection.js
@@ -1,3 +1,5 @@
+// FeatureSection.js
+
 import React from "react";
 import FeatureCard from "./FeatureCard.js";
 import "./FeatureSection.css";
@@ -13,64 +15,61 @@ let FeatureSection = () => {
         </p>
       </div>
       <div className="feature-section__cards">
-        {/* Using React.Fragment to wrap multiple elements */}
-        <React.Fragment>
-          <FeatureCard
-            title="Edge Network"
-            description={
-              <>
-                <span>
-                  Discover our industry leading edge network, with over 300 edge
-                  nodes scattered around the globe.
-                </span>
-                <br />
-                <br />
-                <span>
-                  Give your customers the best performance in every corner of the
-                  world.
-                </span>
-              </>
-            }
-            imageUrl="/imgs/edge-network-img.PNG"
-          />
-          <FeatureCard
-            title="Security"
-            description={
+        <FeatureCard
+          title="Edge Network"
+          description={
+            <>
               <span>
-                Stay safe from threats without slowing down.
-                <br /><br/>
-                SuperNet surrounds and protects your entire ecosystem — clouds,
-                apps, APIs, and users.
+                Discover our industry leading edge network, with over 300 edge
+                nodes scattered around the globe.
               </span>
-            }
-            imageUrl="/imgs/security-img.PNG"
-          />
+              <br />
+              <br />
+              <span>
+                Give your customers the best performance in every corner of the
+                world.
+              </span>
+            </>
+          }
+          imageUrl="/imgs/edge-network-img.PNG"
+        />
+        <FeatureCard
+          title="Security"
+          description={
+            <span>
+              Stay safe from threats without slowing down.
+              <br /><br/>
+              SuperNet surrounds and protects your entire ecosystem — clouds,
+              apps, APIs, and users.
+            </span>
+          }
+          imageUrl="/imgs/security-img.PNG"
+        />
 
-          <FeatureCard
-            title="DDoS Protection"
-            description={
-              <span>
-                Highly rated web, application & network DDoS protection.
-                <br /><br/>
-                Keeping bots and malicious users at bay.
-              </span>
-            }
-            imageUrl="/imgs/DDOS-img.PNG"
-          />
+        <FeatureCard
+          title="DDoS Protection"
+          description={
+            <span>
+              Highly rated web, application & network DDoS protection.
+              <br /><br/>
+              Keeping bots and malicious users at bay.
+            </span>
+          }
+          imageUrl="/imgs/DDOS-img.PNG"
+        />
 
-          <FeatureCard
-            title="Backups"
-            description={
-              <span>
-                Fully managed automatic daily, weekly, and biweekly backups of
-                your Compute Instances.
-                <br /><br/>
-                Have peace of mind knowing that your data is always safe.
-              </span>
-            }
-            imageUrl="/imgs/backups-img.PNG"
-          />
-        </React.Fragment>
+        <FeatureCard
+          title="Backups"
+          description={
+            <span>
+              Fully managed automatic daily, weekly, and biweekly backups of
+              your Compute Instances.
+              <br /><br/>
+              Have peace of mind knowing that your data is always safe.
+            </span>
+          }
+          imageUrl="/imgs/backups-img.PNG"
+        />
       </div>
     </section>
   );
