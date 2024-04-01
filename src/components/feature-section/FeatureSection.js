2d1d3edc9db28dfc56c86ced39eb1ef49b6254c2
Maisie <maisieobb@googlemail.com>
2024-04-01 21:25:43 +0100

    move font sizes to global css, add more box shadow, change margin top of intro section in mob

--- src/components/feature-section/FeatureSection.js
+++ src/components/feature-section/FeatureSection.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import FeatureCard from "../feature-card/FeatureCard";
-import "./Feature-Section.css";
+import "./FeatureSection.css";
 
 let FeatureSection = () => {
   return (
